637b5ea0022a25febce6b490d128665b3a4ac329
omondidenzel <omondiakwany@gmail.com>
2025-03-20 16:18:07 +0200

    final commit

--- etl.py
+++ etl.py
@@ -1,9 +1,10 @@
 import logging
-from dotenv import find_dotenv, load_dotenv
-import sys
 import os
+import sys
 
 import pandas as pd
+import psycopg2
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 
 # Set up logging
@@ -26,7 +27,10 @@ DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT")
 
 # Create database connection
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+engine = create_engine(
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 # Load the Excel file
 file_path = "data.xlsx"
@@ -36,13 +40,15 @@ xls = pd.ExcelFile(file_path)
 for sheet_name in xls.sheet_names:
     df = pd.read_excel(xls, sheet_name=sheet_name)  # Read sheet into DataFrame
     table_name = sheet_name.lower()  # Covert table name to lowercase
-    
+
     try:
         with engine.connect() as con:
-            df.to_sql(table_name, engine, if_exists="replace", index=False)  # Load to DB
+            log.info(f"Connected to db")
+            df.to_sql(
+                table_name, engine, if_exists="replace", index=False
+            )  # Load to DB
             log.info(f"Loaded sheet '{sheet_name}' into table '{table_name}'")
     except Exception as e:
-        log.error(f"Failed to load sheet '{sheet_name}' into table '{table_name}'")
-        log.error(e)
+        log.error(f"Failed to connect to DB: {e}")
 
 # connection will be closed automatically
